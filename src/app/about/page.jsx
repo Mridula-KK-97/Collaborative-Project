7a9a8aa2aee1b1fa0d84ecf16b677e3395c0bb85
mridula <mridula8.97@gmail.com>
2025-07-24 16:41:29 +0530

    collab project

---
+++ src/app/about/page.jsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+
+const page = () => {
+  return (
+    <div>
+        <Link href="\">Go to Home</Link>
+    </div>
+  )
+}
+
+export default page

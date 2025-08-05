1657adbffd3e5539c03e4e41a88db0254fc747da
mridula <mridula8.97@gmail.com>
2025-08-05 16:50:43 +0530

    user

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -4,4 +4,6 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 const supabaseservice = process.env.SUPABASE_SERVICE_ROLE_KEY 
 
-export const supabase = createClient(supabaseUrl, supabaseservice)
+
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
